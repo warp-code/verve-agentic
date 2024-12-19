6964f908ffbf38c43062d4f3a5d9bc33ba24d438
Pileks <pilex13@gmail.com>
2024-12-19 18:02:37 +0100

    wip example

---
+++ example/eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignores: [
+      "coverage/",
+      "lib/",
+      "node_modules/",
+      "tsconfig.vitest-temp.json",
+    ],
+  },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      // enforce linebreak style and fix it automatically with lint:fix
+      "linebreak-style": ["error", "unix"],
+
+      // underscore to ignore unused vars
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
